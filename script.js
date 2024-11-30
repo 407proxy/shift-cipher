df4cb2c926c76e936ff1e6d8789cc4a89cb97bee
407proxy <135014338+407proxy@users.noreply.github.com>
2024-11-30 22:36:38 +0800

    Add files via upload
    created the framework for the shift-cipher website

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirectToDevsPage() {
+    window.location.href = "devs.html"; 
+}
+
+function redirectToHomePage() {
+    window.location.href = "index.html"; 
+}
+
+function redirectToChoice() {
+    window.location.href = "choice.html"; 
+}
